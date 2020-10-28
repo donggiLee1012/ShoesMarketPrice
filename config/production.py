bf31013147e1ec424048762fc2587dd3c44880d5
donggi <multidong1017@gmail.com>
2020-10-28 18:30:08 +0900

    config 변경

--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'mkpf.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xd5\x13\xdb\x8d\x81\xf6\xb8\xfa\x97\x8e\xcbYtHDs'
